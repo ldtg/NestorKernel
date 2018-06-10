1906bf4b66367c1aedd8d89b58ae0b4770372bf4
DMaitia <dariusmaitia@gmail.com>
2018-06-10 14:38:14 -0300

    Primer commit

---
+++ cmake-build-debug/CMakeFiles/Kern2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kern2.dir/main.c.o"
+  "Kern2.pdb"
+  "Kern2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Kern2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
